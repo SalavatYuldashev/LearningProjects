f72e74903b958b4b1d4b287f3e08f9dc6adb5c13
SalavatYuldashev <salavat.yuldashev@vc.com>
2022-06-07 17:12:07 +0300

    Add new web project.

---
+++ Web/WebApplication2019/WebApplication2019/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2019.Models;
+
+namespace WebApplication2019.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration Configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            Configuration = configuration;
+        }
+        public IActionResult PrintInfo()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            var adminName = Configuration.GetSection("Admin:Name");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
