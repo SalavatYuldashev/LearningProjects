8609de140540fd3808a2418d9595a7ed9b1713b2
SalavatYuldashev <salavat.yuldashev@vc.com>
2022-06-07 20:02:51 +0300

    create new branch

---
+++ Web/Automarket/Automarket.Domain/Enum/TypeCar.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Automarket.Domain.Enum
+{
+    public enum TypeCar
+    {
+    [Display(Name = "Седан")]
+    Sedan = 0,
+    [Display(Name = "Внедорожник")]
+    Suv = 1,
+    [Display(Name = "Минивен")]
+    Minivan = 2,
+    [Display(Name = "Кабриолет")]
+    Cabriolet = 3,
+    [Display(Name = "Пикап")]
+    PickupTruck = 4,
+    [Display(Name = "Лимузин")]
+    Limousine = 5
+    }
+}
